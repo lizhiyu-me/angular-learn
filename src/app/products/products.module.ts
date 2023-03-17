52d618ce3c2623526b11e68551963803b8fdc813
SteffanLee <steffan@SteffanLees-MacBook-Air.local>
2023-03-17 08:14:28 +0800

    chore:update to 15.x

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDescComponent } from './product-desc/product-desc.component';
+import { ProductComponent } from './product/product.component';
 
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ProductDescComponent
+    ProductDescComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductComponent
   ],
   exports:[
     ProductListComponent
