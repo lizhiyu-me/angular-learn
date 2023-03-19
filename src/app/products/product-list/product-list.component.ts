0021b36877deacd28f5e2c28e7f030a25ca6c01b
SteffanLee <steffan@SteffanLees-MacBook-Air.local>
2023-03-19 23:00:33 +0800

    chore:style binding

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+  innerText:string = "hello productlist, you are working"
+  currentStyle={
+    color:"yellowgreen",
+    fontSize:"100px"
+  }
   constructor() { }
 
   ngOnInit(): void {
