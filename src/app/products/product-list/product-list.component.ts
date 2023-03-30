70690bc530843838fe0b528bfb0e7ee209d98f1d
SteffanLee <steffan@SteffanLees-MacBook-Air.local>
2023-03-30 08:33:14 +0800

    add:service

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,43 +1,33 @@
+import { ProductsService } from './../products.service';
 import { Product } from './../../product';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements AfterViewInit {
+export class ProductListComponent implements AfterViewInit, OnInit {
+    constructor(private productsService: ProductsService) {
 
-  products: Product[] = [
-    {
-      name: "Webcam",
-      price: 66
-
-    },
-    {
-      name: "Microphone",
-      price: 66
-
-    },
-    {
-      name: "Wireless KeyBoard",
-      price: 66
-
-    },
-  ]
-  today:Date = new Date();
-  selectedProduct:Product|undefined;
-  @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
-
-  ngAfterViewInit(): void {
-    if (this.productDetail) {
-      console.log("this.productDetail product", this.productDetail.product);
     }
-  }
+    ngOnInit(): void {
+        this.products = this.productsService.getProducts();
+    }
+    products: Product[] = [];
+    today: Date = new Date();
+    selectedProduct: Product | undefined;
+    @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
 
-  onBuy(selectedProduct: Product) {
-    window.alert(`You just bought ${selectedProduct.name}!`);
-  }
+    ngAfterViewInit(): void {
+        if (this.productDetail) {
+            console.log("this.productDetail product", this.productDetail.product);
+        }
+    }
+
+    onBuy(selectedProduct: Product) {
+        window.alert(`You just bought ${selectedProduct.name}!`);
+    }
 
 }
